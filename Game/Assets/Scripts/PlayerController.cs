d0755d2aab703194b252cba83f865edb1c7d266f
Marcin7373 <skwarek.marcin@mydit.ie>
2019-11-18 10:14:41 +0000

    Animation sprites updated+crouch+jump animation segmented

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,9 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck, cameraTarget;
     public LayerMask groundLayer;
     public float speed, jumpHeight;
-    private float move, cameraOffset = 3;
-    private bool jump, run, isGrounded, facingRight, isFalling;
+    private float move, cameraOffset = 3, dashCooldown = 0;
+    private bool jump, run, isGrounded, facingRight, isFalling, crouch, dash, busy = false;
+    public float savedVelocity;
 
     public float fallMult = 1.5f;
     public float lowJumpMult = 0.1f;
@@ -25,28 +26,49 @@ public class PlayerController : MonoBehaviour
     {
         //Input
         move = Input.GetAxisRaw("Horizontal");
+        crouch = Input.GetKey("n");
+        if (!busy)
+        {
+            dash = Input.GetKeyDown("b");
+        }
         jump = Input.GetButton("Jump");
         run = Input.GetButton("Fire1");
-       
+
         //inverting sprite for facing direction
-        if (move > 0){
-            facingRight = true;
+        if (move < 0){
+            facingRight = false;
             transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
         }
-        else if (move < 0){
-            facingRight = false;
+        else if (move > 0){
+            facingRight = true;
             transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
         }
 
         //Animations
+        if (busy)
+        {
+            animator.Play("Dash");
+        }
         if (move != 0 && !run && isGrounded){
             animator.Play("Walk");
         }
         else if (run && move != 0 && isGrounded){
             animator.Play("Run");
         }
-        else if (!isGrounded){
+        else if (!isGrounded && rb.velocity.y < 6 & rb.velocity.y > -4)
+        {
+            animator.Play("JumpPeak");
+        }
+        else if (!isGrounded && !isFalling){
             animator.Play("Jump");
+        }
+        else if (isFalling)
+        {
+            animator.Play("Falling");
+        }
+        else if (crouch)
+        {
+            animator.Play("Crouch");
         }
         else{
             animator.Play("Idle");
@@ -58,15 +80,15 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         //variable hight and faster fall
-        if (rb.velocity.y < 0){
+        if (rb.velocity.y < 0) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * fallMult * Time.deltaTime;
             isFalling = true;
         }
-        else if (rb.velocity.y > 0 && !jump){
+        else if (rb.velocity.y > 0 && !jump) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * lowJumpMult * Time.deltaTime;
         }
 
-        if (Physics2D.Linecast(transform.position, groundCheck.position, groundLayer)){
+        if (Physics2D.Linecast(transform.position, groundCheck.position, groundLayer)) {
             isGrounded = true;
             isFalling = false;
         }
@@ -74,6 +96,28 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
 
+        /*if (dash)
+        {
+            savedVelocity = rb.velocity.x;
+            rb.velocity = new Vector2(move * speed * 5, rb.velocity.y);
+            busy = true;
+        }
+        else if (busy)
+        {
+            dash = false;
+            dashCooldown += Time.deltaTime * 3;
+            if (dashCooldown >= 20f)
+            {
+                dashCooldown = 20f;
+                rb.velocity = new Vector2(savedVelocity, rb.velocity.y);
+                busy = false;
+                dashCooldown = 0;
+            }
+        }
+        Debug.Log(dashCooldown);
+        Debug.Log(dash);
+        Debug.Log(busy);
+        */
         if (run) //running
         {
             rb.velocity = new Vector2(move * speed * 3, rb.velocity.y);
@@ -101,6 +145,13 @@ public class PlayerController : MonoBehaviour
         {
             cameraTarget.position = new Vector2(transform.position.x - 1 +(move * cameraOffset), cameraHeight);
         }
-        
     }
+
+    void Dash()
+    {
+        
+        //rb.velocity = new Vector2(move * speed * 5, rb.velocity.y);
+    }
+
+    
 }
