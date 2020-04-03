93d44c7b4688b07f7b75dfebcb26d4dd86a32403
Marcin7373 <skwarek.marcin@mydit.ie>
2020-04-03 17:55:32 +0100

    re-added timepassed back into seeplayer, cleaned up some boss animations on neck box collider

--- Game/Assets/Scripts/BossAI/Scorers/SeePlayer.cs
+++ Game/Assets/Scripts/BossAI/Scorers/SeePlayer.cs
@@ -14,7 +14,7 @@ public class SeePlayer : ContextualScorerBase
     {
         var c = (AIContext)context;
 
-        return ((c.bossTr.position.x+1 >= ((Vector3)c.playerInfo["position"]).x && c.bossTr.rotation.y == 1) || (c.bossTr.position.x-1 < ((Vector3)c.playerInfo["position"]).x && c.bossTr.rotation.y == 0))
+        return (((float)c.playerInfo["timePassed"] > minTimePassed) || (c.bossTr.position.x+1 >= ((Vector3)c.playerInfo["position"]).x && c.bossTr.rotation.y == 1) || (c.bossTr.position.x-1 < ((Vector3)c.playerInfo["position"]).x && c.bossTr.rotation.y == 0))
             ? (score = scoreMin)
             : (score = scoreMax);
     }
